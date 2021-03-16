4909bb35c355e806a06d290ab4c1f1dd7c48f1d5
HP <rizqihza@student.telkomuniversity.ac.id>
2021-03-16 19:24:17 +0800

    Membuat fragment lihat saran

--- app/src/main/java/org/d3if4001/hitungbmi/MainActivity.kt
+++ app/src/main/java/org/d3if4001/hitungbmi/MainActivity.kt
@@ -2,7 +2,6 @@ package org.d3if4001.hitungbmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.d3if4001.hitungbmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/org/d3if4001/hitungbmi/data/KategoriBmi.kt
@@ -0,0 +1,3 @@
+package org.d3if4001.hitungbmi.data
+
+enum class KategoriBmi {KURUS, IDEAL, GEMUK}
